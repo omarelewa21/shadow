01a08ac7f9381185fa055c40df21235bbecce724
Omar Elewa <omarelewa25@gmail.com>
2023-11-25 16:24:58 +0300

    initializing base component

---
+++ resources/views/components/base.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Shadow</title>
+    <!-- Favicons Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/favicons/apple-touch-icon.png')}}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicons/favicon-32x32.png')}}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicons/favicon-16x16.png')}}" />
+    <link rel="manifest" href="{{asset('images/favicons/site.webmanifest')}}" />
+    <meta name="description" content="Baosh HTML 5 Template " />
+
+    <!-- Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('vendors/animate/animate.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/animate/custom-animate.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/bootstrap/css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/bootstrap-select/css/bootstrap-select.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/fontawesome/css/all.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/jquery-magnific-popup/jquery.magnific-popup.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/jquery-ui/jquery-ui.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/nice-select/nice-select.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/odometer/odometer.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/owl-carousel/owl.carousel.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/owl-carousel/owl.theme.default.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/swiper/swiper.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/thm-icons/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendors/reey-font/stylesheet.css')}}" />
+
+    <!-- template styles -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}" />
+</head>
+
+<body>
+    
+
+
+
+
+
+    <script src="{{asset('vendors/jquery/jquery-3.6.0.min.js')}}" ></script>
+    <script src="{{asset('vendors/bootstrap/js/bootstrap.bundle.min.js')}}" ></script>
+    <script src="{{asset('vendors/bootstrap-select/js/bootstrap-select.min.js')}}" ></script>
+    <script src="{{asset('vendors/isotope/isotope.js')}}" ></script>
+    <script src="{{asset('vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js')}}" ></script>
+    <script src="{{asset('vendors/jquery-appear/jquery.appear.min.js')}}" ></script>
+    <script src="{{asset('vendors/jquery-magnific-popup/jquery.magnific-popup.min.js')}}" ></script>
+    <script src="{{asset('vendors/jquery-ui/jquery-ui.js')}}" ></script>
+    <script src="{{asset('vendors/jquery-validate/jquery.validate.min.js')}}" ></script>
+    <script src="{{asset('vendors/nice-select/jquery.nice-select.min.js')}}" ></script>
+    <script src="{{asset('vendors/odometer/odometer.min.js')}}" ></script>
+    <script src="{{asset('vendors/owl-carousel/owl.carousel.min.js')}}" ></script>
+    <script src="{{asset('vendors/swiper/swiper.min.js')}}" ></script>
+    <script src="{{asset('vendors/wnumb/wNumb.min.js')}}" ></script>
+    <script src="{{asset('vendors/wow/wow.js')}}" ></script>
+    <script src="{{asset('vendors/jarallax/jarallax.min.js')}}" ></script>
+    <script src="{{asset('vendors/sidebar-content/jquery-sidebar-content.js')}}" ></script>
+    <script src="{{asset('vendors/gsap/gsap.js')}}" ></script>
+    <script src="{{asset('vendors/gsap/ScrollTrigger.js')}}" ></script>
+    <script src="{{asset('vendors/gsap/SplitText.js')}}" ></script>
+
+    <!-- Template js -->
+    <script src="{{asset('js/custom.js')}}" ></script>
+</body>
+
+</html>
