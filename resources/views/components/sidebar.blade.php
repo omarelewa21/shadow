504b8132c2e3fa8168223c369ea45a399ab98f39
Omar Elewa <omarelewa25@gmail.com>
2023-11-25 16:51:12 +0300

    making a component for sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,85 @@
+<!-- Start sidebar widget content -->
+<div class="xs-sidebar-group info-group info-sidebar">
+    <div class="xs-overlay xs-bg-black"></div>
+    <div class="xs-sidebar-widget">
+        <div class="sidebar-widget-container">
+            <div class="widget-heading">
+                <a href="#" class="close-side-widget">X</a>
+            </div>
+            <div class="sidebar-textwidget">
+                <div class="sidebar-info-contents">
+                    <div class="content-inner">
+                        <div class="logo">
+                            <a href="index.html"><img src="{{asset('images/resources/logo-1.png')}}" alt="" /></a>
+                        </div>
+                        <div class="content-box">
+                            <h4>About Us</h4>
+                            <div class="inner-text">
+                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+                                    roots in a piece of classical Latin literature from 45 BC, making it over
+                                    2000 years old.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="form-inner">
+                            <h4>Get a free quote</h4>
+                            <form action="index.html" method="post">
+                                <div class="form-group">
+                                    <input type="text" name="name" placeholder="Name" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" placeholder="Email" required="">
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="message" placeholder="Message..."></textarea>
+                                </div>
+                                <div class="form-group message-btn">
+                                    <button class="thm-btn" type="submit" data-loading-text="Please wait...">
+                                        <span class="txt">Submit Now</span>
+                                        <span class="bdrl"></span>
+                                        <span class="bdrr"></span>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="sidebar-contact-info">
+                            <h4>Contact Info</h4>
+                            <ul>
+                                <li>
+                                    <span class="icon-placeholder-1"></span> 88 broklyn street, New York
+                                </li>
+                                <li>
+                                    <span class="icon-phone-ringing"></span>
+                                    <a href="tel:123456789">+1 555-9990-153</a>
+                                </li>
+                                <li>
+                                    <span class="fa fa-envelope"></span>
+                                    <a href="mailto:info@example.com">info@example.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="thm-social-link1">
+                            <ul class="social-box">
+                                <li class="facebook">
+                                    <a href="#"><i class="icon-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li class="twitter">
+                                    <a href="#"><i class="icon-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li class="linkedin">
+                                    <a href="#"><i class="icon-instagram" aria-hidden="true"></i></a>
+                                </li>
+                                <li class="gplus">
+                                    <a href="#"><i class="icon-pinterest" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End sidebar widget content -->
