408e077a01d4fd5cb520e0c7da2f0d0b96c88caa
Omar Elewa <omarelewa25@gmail.com>
2023-11-25 17:15:05 +0300

    creating a component for header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,70 @@
+<header class="main-header main-header-two">
+    <div class="main-header-two__inner">
+        <nav class="main-menu main-menu-one">
+            <div class="main-menu__wrapper clearfix">
+                <div class="auto-container">
+                    <div class="main-menu__wrapper-inner">
+                        <div class="main-header-two__left">
+                            <div class="logo-box-one">
+                                <x-logo></x-logo>
+                            </div>
+                        </div>
+
+                        <div class="main-menu-box">
+                            <a href="#" class="mobile-nav__toggler">
+                                <i class="fa fa-bars"></i>
+                            </a>
+
+                            <ul class="main-menu__list">
+                                <li class="current">
+                                    <a href="index.html">Home</a>
+                                </li>
+
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+
+                                <li class="dropdown">
+                                    <a href="#">Services</a>
+                                    <ul>
+                                        <li><a href="services.html">Services</a></li>
+                                        <li>
+                                            <a href="agriculture-services.html">Agriculture Services</a>
+                                        </li>
+                                        <li>
+                                            <a href="graphic-design.html">Graphic Design</a>
+                                        </li>
+                                        <li>
+                                            <a href="delivery-services.html">Delivery Services</a>
+                                        </li>
+                                        <li>
+                                            <a href="farming-products.html">Farming Products</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li>
+                                    <a href="blog.html">Blog</a>
+                                </li>
+
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="main-header-two__right">
+                            <div class="side-content-button-box">
+                                <div class="side-content-button">
+                                    <a class="navSidebar-button" href="#">
+                                        <span class="icon-menu"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
