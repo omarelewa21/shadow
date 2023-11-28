4e6ef35511f1f843e145855480dd902cba2d8e94
Omar Elewa <omarelewa25@gmail.com>
2023-11-28 15:50:40 +0300

    adding title var

--- app/View/Components/Base.php
+++ app/View/Components/Base.php
@@ -8,17 +8,13 @@ use Illuminate\View\Component;
 
 class Base extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public array $appName;
+
+    public function __construct(public string $title)
     {
-        //
+        $this->appName = str_split(config('app.name'));
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.base');
